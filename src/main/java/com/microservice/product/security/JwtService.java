60228c2e00152d15486be49f01655d4820080ecd
harrison314790 <117177675+harrison314790@users.noreply.github.com>
2025-05-10 11:34:01 -0500

    segundo avance de jwt y roles

---
+++ src/main/java/com/microservice/product/security/JwtService.java
@@ -0,0 +1,23 @@
+package com.microservice.product.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "mi_clave_secreta_segura";
+
+    public String extractUsername(String token) {
+        return JWT.decode(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return username.equals(userDetails.getUsername());
+    }
+}
